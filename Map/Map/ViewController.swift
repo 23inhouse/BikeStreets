6613f10e1dcf103877e2f14aa96c70454c01153f
Benjamin Lewis <23inhouse@gmail.com>
2019-10-03 14:34:20 +0200

    Add base project

---
+++ Map/Map/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Benjamin Lewis on 3/10/19.
+//  Copyright © 2019 Benjamin Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
