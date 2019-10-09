c2b6ce26efbdc44104aa4ec3deff7e68f6c7eaef
Benjamin Lewis <23inhouse@gmail.com>
2019-10-09 18:14:01 +0200

    Add Extensions

---
+++ Map/Map/Extensions/UIViewController+Subcontroller.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewController+Subcontroller.swift
+//  Map
+//
+//  Created by Benjamin Lewis on 9/10/19.
+//  Copyright © 2019 Benjamin Lewis. All rights reserved.
+//
+
+import UIKit
+
+@nonobjc extension UIViewController {
+  func add(_ child: UIViewController, frame: CGRect? = nil) {
+    addChild(child)
+
+    if let frame = frame {
+      child.view.frame = frame
+    }
+
+    view.addSubview(child.view)
+    child.didMove(toParent: self)
+  }
+
+  func remove() {
+    willMove(toParent: nil)
+    view.removeFromSuperview()
+    removeFromParent()
+  }
+}
